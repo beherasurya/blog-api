081099c8ce7891c5bfeac9ffb9a3d0ac86119417
beherasurya <beherasuryakanta18@gmail.com>
2023-07-23 14:21:50 +0530

    Create user implemented in service implementation class

--- blog-api/src/main/java/com/app/blogapi/payloads/UserDto.java
+++ blog-api/src/main/java/com/app/blogapi/payloads/UserDto.java
@@ -9,7 +9,6 @@ import lombok.Setter;
 @NoArgsConstructor
 public class UserDto {
     
-    private int id;
     private String name;
 
     private String email;
--- blog-api/src/main/java/com/app/blogapi/services/serviceimplementation/UserServiceImplementation.java
+++ blog-api/src/main/java/com/app/blogapi/services/serviceimplementation/UserServiceImplementation.java
@@ -2,15 +2,27 @@ package com.app.blogapi.services.serviceimplementation;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.app.blogapi.entities.User;
 import com.app.blogapi.payloads.UserDto;
+import com.app.blogapi.repositories.UserRepository;
 import com.app.blogapi.services.UserService;
 
 public class UserServiceImplementation implements UserService{
 
+
+    @Autowired
+    UserRepository userRepository ;
+
     @Override
     public UserDto createUser(UserDto userDto) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+
+        User user= userDtoToUser(userDto);
+        User savedUser= userRepository.save(user);
+        
+      return  userToUserDto(savedUser);
     }
 
     @Override
@@ -35,6 +47,29 @@ public class UserServiceImplementation implements UserService{
     public UserDto deleteUser(int id) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'deleteUser'");
+    }
+
+    private User userDtoToUser(UserDto userDto){
+
+        User user = new User();
+        user.setName(userDto.getName());
+
+        user.setEmail(userDto.getEmail());
+        user.setAbout(userDto.getAbout());
+
+        user.setPassword(userDto.getPassword());
+        return user;
+    }
+    private UserDto userToUserDto(User user){
+
+        UserDto userDto =new UserDto();
+        userDto.setName(user.getName());
+
+        userDto.setEmail(user.getEmail());
+        userDto.setAbout(user.getAbout());
+
+        userDto.setPassword(user.getPassword());
+        return userDto;
     }
     
 }
