fe37e2ed4b0ae78045b187d01fd6f2b96bcebcea
beherasurya <beherasuryakanta18@gmail.com>
2023-07-25 16:51:48 +0530

    Validation updated

--- blog-api/src/main/java/com/app/blogapi/exceptions/GlobalExceptionHandler.java
+++ blog-api/src/main/java/com/app/blogapi/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.app.blogapi.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,17 +27,23 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+    ResponseEntity<Map<String,String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
 
         // ApiResponse apiResponse = new ApiResponse("Please give Valid input for the  attributes", false);
 
-        String apiResponse = exception.getFieldErrors().stream()
-        .map(fieldError -> fieldError.getField() + " - "+fieldError.getDefaultMessage()).collect(Collectors.joining(" , "));
+        // String apiResponse = exception.getFieldErrors().stream()
+        // .map(fieldError -> fieldError.getField() + " - "+fieldError.getDefaultMessage()).collect(Collectors.joining(" , "));
 
 
-        
-        ApiResponse apiResponse2 = new ApiResponse(apiResponse, false);
-        return new ResponseEntity<ApiResponse>(apiResponse2, HttpStatus.NOT_ACCEPTABLE);
+        Map<String, String> apiResponse = new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach((error) ->{
+            String fieldName = ((FieldError)error).getField();
+            String message = error.getDefaultMessage();
+
+            apiResponse.put(fieldName, message);
+        });
+                
+        return new ResponseEntity<Map<String,String>>(apiResponse, HttpStatus.NOT_ACCEPTABLE);
 
     }
 
