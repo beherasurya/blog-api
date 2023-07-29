fea34b148c5ec40f69684f6ec498eef16aa0ba7d
beherasurya <beherasuryakanta18@gmail.com>
2023-07-29 12:16:44 +0530

    post API updated

--- blog-api/src/main/java/com/app/blogapi/repositories/PostRepository.java
+++ blog-api/src/main/java/com/app/blogapi/repositories/PostRepository.java
@@ -2,8 +2,6 @@ package com.app.blogapi.repositories;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.app.blogapi.entities.Category;
